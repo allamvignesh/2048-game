a025ba3a935245c815284da93282579a3f65bb95
allamvignesh <allam.vignesh@gmail.com>
2024-01-11 23:26:04 +0530

    Fixed boxes

--- script.js
+++ script.js
@@ -246,3 +246,4 @@ document.addEventListener('touchend', e => {
 })
 
 let colors = {0: "#cdc1b4", 2: "#eee4da", 4: "#eee1c9", 8: "#f3b27a", 16: "#f69664", 32: "#f77c5f", 64: "#f75f3b", 128: "#edd073", 256: "#edcc61", 512: "#edc651", 1024: "#eec744", 2048: "#ecc230"}
+colorGrid()
